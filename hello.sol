471d2099d151f523293de1c55f40a1f53b76a333
Joseph Martin <129386460+jfmartinz@users.noreply.github.com>
2023-07-31 20:01:45 +0800

    Update hello.sol

--- hello.sol
+++ hello.sol
@@ -1,14 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract AgeRestrictionClub {
+contract club {
     uint public minimumAge = 18;
 
-    function enterClub(uint age) public view {
-        require(age > 0 && age < 60, "Age must be greater than 0 and less than 60.");
+    function eligibility(uint age) public view {
+        require(age > 10 && age < 60, "Age must be greater than 10 and less than 60.");
         if (age < minimumAge) {
             revert("You must be at least 18 years old to enter the club.");
         }
-        assert(age >= minimumAge && age < 60);
+          assert(age >= minimumAge && age < 60); 
+          revert("You can enter the club");
+        
     }
 }
